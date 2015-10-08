a78e487535f29423da84df9a468c6d40c4a2cdfe
mattdlh <matt@catalina.io>
2015-10-08 12:01:29 -0700

    dockerfile fix and unicorn settings

--- rails-nginx-unicorn/unicorn.rb
+++ rails-nginx-unicorn/unicorn.rb
@@ -8,5 +8,5 @@ stderr_path "#{app_dir}/log/unicorn.stderr.log"
 stdout_path "#{app_dir}/log/unicorn.stdout.log"
  
 worker_processes 1
-listen "/tmp/unicorn.sock", :backlog => 64
-timeout 30
+listen "/tmp/unicorn.sock", :backlog => 4096
+timeout 15
